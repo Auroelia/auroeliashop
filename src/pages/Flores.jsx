4a3a37de263c7f01b49cb134c0ccc208e153639c
hyzmael88 <31554724+hyzmael88@users.noreply.github.com>
2024-04-22 04:32:57 -0600

    Add Flores components: Hero, Productos, and Flores to the project

---
+++ src/pages/Flores.jsx
@@ -0,0 +1,12 @@
+import Hero from '@/components/Flores/Hero/Hero'
+import React from 'react'
+
+function Flores() {
+  return (
+    <div>
+        <Hero/>
+    </div>
+  )
+}
+
+export default Flores
