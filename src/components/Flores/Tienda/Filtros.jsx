5e0af9b43b18ef688676ca003c3c3dfb9e8462b1
hyzmael88 <31554724+hyzmael88@users.noreply.github.com>
2024-04-22 05:22:43 -0600

    Refactor Flores components and update dependencies

---
+++ src/components/Flores/Tienda/Filtros.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Filtros() {
+  return (
+    <div className='w-[220px] flex flex-col bg-red-500'>
+        <span>Filtros</span>
+        <hr/>
+        
+    </div>
+  )
+}
+
+export default Filtros
--- src/pages/Flores.jsx
+++ src/pages/Flores.jsx
@@ -1,10 +1,12 @@
 import Hero from '@/components/Flores/Hero/Hero'
+import Layout from '@/components/Flores/Tienda/Layout'
 import React from 'react'
 
 function Flores() {
   return (
     <div>
         <Hero/>
+        <Layout/>
     </div>
   )
 }
