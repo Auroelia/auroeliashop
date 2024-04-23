b3c1054d8e9ec93c3b52fefff6b7eb1ee92952ac
hyzmael88 <31554724+hyzmael88@users.noreply.github.com>
2024-04-23 06:12:19 -0600

    Refactor Filtros.jsx and Productos.jsx components

--- src/components/Flores/Tienda/Filtros.jsx
+++ src/components/Flores/Tienda/Filtros.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 function Filtros() {
   return (
-    <div className='w-[220px] flex flex-col bg-red-500'>
-        <span>Filtros</span>
-        <hr/>
+    <div className='w-[220px] flex flex-col '>
+        <span>Filtrar</span>
+        <div className='bg-[#E39C9D] h-[1px] w-full'/>
         
     </div>
   )
--- src/components/Flores/Tienda/Productos.jsx
+++ src/components/Flores/Tienda/Productos.jsx
@@ -1,10 +1,12 @@
 import React, { useEffect, useState } from 'react';
-import client from '../../../../sanity/lib/client'
+import {client} from '../../../../sanity/lib/client'
+import { urlForImage } from '../../../../sanity/lib/image';
 
 
 
 function Productos() {
   const [productos, setProductos] = useState([]);
+  console.log(productos)
 
   useEffect(() => {
     // Realiza la consulta a la API de Sanity para obtener los productos
@@ -15,9 +17,28 @@ function Productos() {
   }, []);
 
   return (
-    <div>
+    <div className='w-full h-full grid grid-cols-3 gap-x-[22px] gap-y-[61px] place-items-center'>
       {productos.map((producto) => (
-        <div key={producto._id}>{producto.nombre}</div>
+        <div key={producto._id} className='w-[229px] shadow-popular rounded-[30px]'>
+          <img src={urlForImage(producto?.imagenes[0]?.asset._ref)} alt={producto.nombre}
+          className='w-full h-[263px] object-cover rounded-t-[30px]'
+          />
+          <div className='h-[100px]  flex flex-col  justify-center px-[22px]'>
+            <span className='font-inter font-bold text-[16px]'>
+          {producto.nombre}
+            </span>
+            <div className='flex justify-between items-center '>
+              <span className='font-inter font-bold text-[16px]'>
+                ${producto.precio}.00
+                </span>
+               
+
+                <img src="/assets/icons/carrito.svg" alt="carrito de compras" className='w-[30px] h-[30px]' />
+             
+
+              </div>
+          </div>
+          </div>
       ))}
     </div>
   );
