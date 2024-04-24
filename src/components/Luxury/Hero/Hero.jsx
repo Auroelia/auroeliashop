0fa887e5f407a13c1c951772b9cc80095edb9290
hyzmael88 <31554724+hyzmael88@users.noreply.github.com>
2024-04-24 16:34:26 -0600

    Refactor image paths in Hero.jsx and Luxury.jsx components

--- src/components/Luxury/Hero/Hero.jsx
+++ src/components/Luxury/Hero/Hero.jsx
@@ -3,15 +3,15 @@ import React from 'react'
 function Hero() {
     return (
         <div className='h-[900px] w-full relative '>
-                <img src='/assets/Flores/floresHero.jpg' alt='hero' className='w-full h-[493px] object-cover ' />
+                <img src='/assets/Luxury/heroLuxury.jpg' alt='hero' className='w-full h-[493px] object-cover ' />
                 <div className='w-full h-[493px] absolute top-0 bg-black/60'/>
                 <div className='h-[348px] w-[840px] rounded-[58px] absolute bg-white bottom-[120px] left-0 right-0 mx-auto flex flex-col items-center justify-center'
                 style={{boxShadow: '0px 4px 40.6px 0px rgba(0, 0, 0, 0.12)'}}>
                 <span className="text-[#E39C9D] font-inter font-bold text-[32px]">
-                Nuestras Flores
+                Arreglos Lujosos
                 </span>
-                <h2 className="text-[83px]">Ideas del corazón</h2>
-                <p className='text-center w-[671px]'>Descubre nuestra colección única de arreglos florales artesanales, hechos a mano con creatividad y calidad. Añade encanto a cualquier ocasión con nuestras obras maestras florales. Utilizamos una variedad de materiales seleccionados cuidadosamente, como el jabón, para ofrecerte una experiencia verdaderamente encantadora.
+                <h2 className="text-[83px]">Elegancia Floral</h2>
+                <p className='text-center w-[671px]'>Descubre nuestra exclusiva colección de arreglos florales de lujo, donde la elegancia y lo artesanal se fusionan en cada detalle. Desde impresionantes arreglos de novia hasta encantadores Ositos Fancy, celebra tus momentos especiales con la belleza incomparable de nuestras creaciones artesanales.
 </p>
                 </div>
         </div>
--- src/pages/Luxury.jsx
+++ src/pages/Luxury.jsx
@@ -1,4 +1,4 @@
-import Hero from '@/components/Flores/Hero/Hero'
+import Hero from '@/components/Luxury/Hero/Hero'
 import Layout from '@/components/Flores/Tienda/Layout'
 import React from 'react'
 
