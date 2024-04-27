ad001c641776df76de71f97bba4528c123c6c3fa
hyzmael88 <31554724+hyzmael88@users.noreply.github.com>
2024-04-27 05:09:19 -0600

    Refactor Navbar component to update logo link and add shopping cart icon

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,29 +5,32 @@ import Link from 'next/link'
 function Navbar() {
   return (
     <div className='h-[120px] w-full flex flex-row justify-center items-center gap-x-[52px]'>
-      <div>
+      <Link href={"/"}>
         <Image src='/assets/logonav.png' width={100} height={100} alt='logo' />
-      </div>
+      </Link>
       <div>
         <ul className='flex flex-row gap-x-[49px]'>
-          <li>
+          <li className='text-[15px] font-medium hover:text-[#E39C9D] hover:scale-125 transition-all duration-300'>
             <Link href={"/Acerca"}>Acerca</Link>
           </li>
           
-          <li>
+          <li className='text-[15px] font-medium hover:text-[#E39C9D] hover:scale-125 transition-all duration-300'>
             <Link href={"/Flores"}>Flores</Link>
           </li>
-          <li>
+          <li className='text-[15px] font-medium hover:text-[#E39C9D] hover:scale-125 transition-all duration-300'>
             <Link href={"/Luxury"}>Luxury</Link>
           </li>
-          <li>
+          <li className='text-[15px] font-medium hover:text-[#E39C9D] hover:scale-125 transition-all duration-300'>
             <Link href={"/Contacto"}>Contacto</Link>
           </li>
         </ul>
       </div>
       <div className='flex gap-[19px]'>
-        <Link href={"/Carrito"} >
+        <Link href={"/Carrito"} className='relative'  >
         <Image src="/assets/icons/carrito.svg" width={30} height={30} className='cursor-pointer w-[20px] h-[22px]' alt="carrito de compras" />
+        <div className='absolute top-0 -right-[7px] rounded-full w-[10px] h-[10px] bg-[#E39C9D]'>
+
+        </div>
         </Link>
         <Image src="/assets/icons/fb.svg" width={30} height={30}
         className='cursor-pointer w-[13px] h-[21px]'
