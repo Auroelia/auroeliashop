b9aa030c42b6c5486e5398ccdaa78448fda60c60
hyzmael88 <31554724+hyzmael88@users.noreply.github.com>
2024-05-01 23:31:26 -0600

    Update image paths and sizes in Mision.jsx component

--- src/components/Acerca/Mision/Mision.jsx
+++ src/components/Acerca/Mision/Mision.jsx
@@ -3,13 +3,13 @@ import React from "react";
 function Mision() {
   return (
     <div className="w-full h-full flex flex-col gap-[81px] relative">
-      <div className="w-full h-full flex flex-row justify-center items-center gap-[35px]">
+      <div className="w-full h-full flex flex-row flex-wrap justify-center items-center gap-[25px] lg:gap-[35px]">
         <div
-          className="w-[325px] h-[341px] bg-[#E39C9D] flex flex-col items-center justify-center text-center"
+          className="w-[180px] h-[190px] lg:w-[325px] lg:h-[341px] rounded-[3px] bg-[#E39C9D] flex flex-col items-center justify-center text-center"
           style={{ boxShadow: "0px 4px 40.6px 0px rgba(0, 0, 0, 0.12)" }}
         >
           <span className="font-bold font-inter text-[36px]">Misión</span>
-          <p className="text-white text-[16px] leading-[23px] w-[241px] rounded-[6px] h-[200px]">
+          <p className="text-white  text-[10px] lg:text-[16px] leading-[12px] lg:leading-[23px] w-[133px] lg:w-[241px] rounded-[6px] h-[200px]">
             Nos comprometemos a transformar momentos ordinarios en experiencias
             extraordinarias a través de la belleza y la creatividad de nuestros
             arreglos florales artesanales, ofreciendo productos exclusivos y de
@@ -17,13 +17,13 @@ function Mision() {
           </p>
         </div>
         <div
-          className="w-[325px] h-[341px] bg-black flex flex-col items-center justify-center text-center"
+          className="w-[180px] h-[190px] lg:w-[325px] lg:h-[341px] rounded-[3px] bg-black flex flex-col items-center justify-center text-center"
           style={{ boxShadow: "0px 4px 40.6px 0px rgba(0, 0, 0, 0.12)" }}
         >
           <span className="font-bold text-[#E39C9D] font-inter text-[36px]">
             Visión
           </span>
-          <p className="text-white text-[16px] leading-[23px] w-[241px] rounded-[6px] h-[200px]">
+          <p className="text-white text-[10px] lg:text-[16px] leading-[12px] lg:leading-[23px] w-[133px] lg:w-[241px] rounded-[6px] h-[200px]">
             Buscamos ser líderes en el mercado de arreglos florales de lujo,
             reconocidos por nuestra elegancia, originalidad y compromiso con la
             excelencia. Aspiramos a ser la elección preferida de aquellos que
@@ -32,14 +32,14 @@ function Mision() {
           </p>
         </div>
         <div
-          className="w-[325px] h-[341px]  flex flex-col items-center justify-center text-center"
+          className="w-[180px] h-[190px] lg:w-[325px] lg:h-[341px] rounded-[3px]  flex flex-col items-center justify-center text-center"
           style={{ boxShadow: "0px 4px 40.6px 0px rgba(0, 0, 0, 0.12)" }}
         >
           <span className=" text-[#E39C9D] font-bold font-inter text-[36px]">
             Valores
           </span>
 
-          <ul className=" text-[16px] leading-[23px] w-[241px] h-[200px] rounded-[6px]">
+          <ul className=" text-[10px] lg:text-[16px] leading-[12px] lg:leading-[23px] w-[132px] lg:w-[241px] lg:h-[200px] h-[1100px] rounded-[6px]">
             <li>Creatividad</li>
             <li>Originalidad</li>
             <li>innovación</li>
