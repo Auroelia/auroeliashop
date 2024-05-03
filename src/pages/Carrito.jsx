3ebbadc2bd70431bfa6d0e606919881cc59d66c4
hyzmael88 <31554724+hyzmael88@users.noreply.github.com>
2024-05-03 04:44:42 -0600

    Update image paths and sizes in Carrito/Productos.jsx and pages/Carrito.jsx components

--- src/pages/Carrito.jsx
+++ src/pages/Carrito.jsx
@@ -44,20 +44,20 @@ const [descuento, setDescuento] = useState(0)
   return (
     <div className="w-full h-full overflow-hidden relative">
       <div
-        className="w-full max-w-[1440px] min-w-sm mx-auto flex flex-col lg:flex-row justify-center items-center gap-[33px]
+        className="w-full max-w-[1440px] min-w-sm mx-auto flex flex-col lg:flex-row justify-center items-center gap-[33px] px-4 lg:px-0 mt-4
       
       "
       >
         {/* Productos */}
         <div className="lg:w-[639px] flex flex-col justify-center items-center">
-          <div className="w-full grid grid-cols-4 lg:grid-cols-5 place-items-center">
+          <div className="w-full hidden lg:grid grid-cols-5 place-items-center">
             <div>{/* vacio */}</div>
             <div>Producto</div>
             <div className="hidden lg:block">Cantidad</div>
             <div>{/* vacio */}</div>
             <div>Total</div>
           </div>
-          <div className="h-[1px] bg-[#E39C9D] w-full" />
+          <div className="h-[1px] bg-[#E39C9D] w-full hidden lg:block" />
           {
           cart.length>0?
           cart.map((item, index) => (
