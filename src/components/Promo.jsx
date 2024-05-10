70a7e64edebaf5ffdb5c2f0f222edb1c1100d9e2
hyzmael88 <bartttt15@hotmail.com>
2024-05-10 02:19:38 -0300

    Update promo text in Promo component

--- src/components/Promo.jsx
+++ src/components/Promo.jsx
@@ -13,8 +13,8 @@ function Promo() {
       ) : (
         <>
     <div className=' w-full h-[35px] bg-[#E39C9D] flex flex-row justify-center items-center mt-[20px] lg:mt-[0px] '>
-      <p className='text-[12px] font-bold uppercase text-center '>
-      FLASH PROMO 10% SOLO HOY USANDO EL CUPÓN AUROELIANA
+      <p className='text-[12px] font-bold  text-center '>
+      FLASH PROMO 10% SOLO HOY USANDO EL CUPÓN 10DeMayo
       </p>
       </div>
       </>
