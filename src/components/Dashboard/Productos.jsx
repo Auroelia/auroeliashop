3eb6fdb828800c1c5b3a73364de6e5b399b51364
hyzmael88 <31554724+hyzmael88@users.noreply.github.com>
2024-05-17 17:20:15 -0600

    Update image paths and sizes in various components

--- src/components/Dashboard/Productos.jsx
+++ src/components/Dashboard/Productos.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
-
+import Iframe from 'react-iframe'
 function Productos() {
   return (
-    <iframe src='http://localhost:3000/studio/structure/producto'
+    <Iframe src='https://auroelia.com/studio/structure/producto'
     className='w-full h-full'
     title='Productos Studio'
     />
