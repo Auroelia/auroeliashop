d2ac29d8bdcde4b95c3cb70c2950e2249294cfad
hyzmael88 <31554724+hyzmael88@users.noreply.github.com>
2024-08-29 17:58:42 -0600

    chore: Update image paths and sizes in Flores/Tienda, AR, Faqs, Filtros, and [productSlug] components

--- src/components/Flores/Tienda/Producto.jsx
+++ src/components/Flores/Tienda/Producto.jsx
@@ -1,35 +1,39 @@
-import React from 'react'
-import { urlForImage } from '../../../../sanity/lib/image'
-import { useRouter } from 'next/router'
-import Image from 'next/image';
+import React, { useState } from 'react';
+import { urlForImage } from '../../../../sanity/lib/image';
+import { useRouter } from 'next/router';
+import Skeleton from 'react-loading-skeleton';
+import 'react-loading-skeleton/dist/skeleton.css';
 
-function Producto({producto, addToCart}) {
+function Producto({ producto, addToCart }) {
+  const router = useRouter();
+  const [imageLoaded, setImageLoaded] = useState(false);
 
-  const router = useRouter()
+  const handleImageLoad = () => {
+    console.log('Image loaded');
+    setImageLoaded(true);
+  };
 
   return (
-    <div
-      className="w-full md:w-[70%] lg:w-[229px] shadow-popular rounded-[30px] cursor-pointer"
-    >
+    <div className="w-full md:w-[70%] lg:w-[229px] shadow-popular rounded-[30px] cursor-pointer">
       {producto ? (
         <>
+          {!imageLoaded && <Skeleton height={200} />}
           <img
-          
             src={urlForImage(producto.imagenes[0].asset._ref)}
             alt={producto.nombre}
-            className="w-full h-[173px] lg:h-[263px] object-cover rounded-t-[30px]"
+            onLoad={handleImageLoad}
+            className={`w-full h-[173px] lg:h-[263px] object-cover rounded-t-[30px] ${imageLoaded ? '' : 'hidden'}`}
             onClick={() => router.push(`/${producto.slug.current}`)}
           />
-          <div className="h-[100px]  flex flex-col  justify-center px-[22px]">
+          <div className="h-[100px] flex flex-col justify-center px-[22px]">
             <span className="font-inter font-bold text-[12px] lg:text-[16px]">
               {producto.nombre}
             </span>
-            <div className="flex justify-between items-center ">
+            <div className="flex justify-between items-center">
               <span className="font-inter font-bold text-[12px] lg:text-[16px]">
-              ${producto?.tamanos?.[producto.tamanos.length - 1]?.precio}.00    
-                        </span>
-
-            {/*   <img
+                ${producto?.tamanos?.[producto.tamanos.length - 1]?.precio}.00
+              </span>
+              {/* <img
                 src="/assets/icons/carrito.svg"
                 alt="carrito de compras"
                 className="w-[20px] h-[20px] lg:w-[30px] lg:h-[30px] cursor-pointer hover:scale-125 transition-all duration-300"
@@ -48,4 +52,4 @@ function Producto({producto, addToCart}) {
   );
 }
 
-export default Producto
+export default Producto;
