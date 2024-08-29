36b7ab570c0278b9170036c8e75cfad403fcb765
hyzmael88 <31554724+hyzmael88@users.noreply.github.com>
2024-08-28 23:53:20 -0600

    chore: Update image paths and sizes in Flores/Tienda, AR, Faqs, Filtros, and [productSlug] components

--- src/components/Flores/Tienda/FiltrosModal.jsx
+++ src/components/Flores/Tienda/FiltrosModal.jsx
@@ -1,54 +1,41 @@
 import { client } from '@/lib/client';
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 import Arreglo from './Arreglo';
 import Flor from './Flor';
 import RangeSlider from "react-range-slider-input";
 import "react-range-slider-input/dist/style.css";
 
-function FiltrosModal({isOpen,close, checklist, setChecklist, checklistArreglos, setChecklistArreglos, handleCheckListChange}) {
-
-  console.log(isOpen)
-
+function FiltrosModal({ isOpen, close, checklist, setChecklist, checklistArreglos, setChecklistArreglos, handleCheckListChange }) {
   const [flores, setFlores] = useState([]);
   const [arreglos, setArreglos] = useState([]);
+  const [tempChecklist, setTempChecklist] = useState(checklist);
+  const [tempChecklistArreglos, setTempChecklistArreglos] = useState(checklistArreglos);
 
   useEffect(() => {
-    // Realiza la consulta a la API de Sanity para obtener los productos
     client
       .fetch('*[_type == "flor"]')
       .then((data) => setFlores(data))
       .catch((error) => console.error(error));
   }, []);
-  
+
   useEffect(() => {
-    // Realiza la consulta a la API de Sanity para obtener los productos
     client
       .fetch('*[_type == "arreglo"]')
       .then((data) => setArreglos(data))
       .catch((error) => console.error(error));
   }, []);
 
-
-  
-
   const [floresOpen, setFloresOpen] = useState(true);
-
   const [arreglosOpen, setArreglosOpen] = useState(true);
-
-
   const [precioOpen, setPrecioOpen] = useState(true);
-
   const [checklistPrecio, setChecklistPrecio] = useState([]);
-
-  const [value, setValue] = useState([0, 100]); 
+  const [value, setValue] = useState([0, 100]);
 
   const handleChange = (newValue) => {
     setValue(newValue);
-  }
+  };
 
-  if (!isOpen) return null; 
-
-  
+  if (!isOpen) return null;
 
   return (
     <div style={{
@@ -64,77 +51,44 @@ function FiltrosModal({isOpen,close, checklist, setChecklist, checklistArreglos,
       zIndex: 1000
     }}>
       <div
-      className='bg-white w-full h-full lg:rounded-[54px] flex flex-col  justify-center relative px-8'
-      style={{
-        boxShadow: '0 4px 6px rgba(0, 0, 0, 0.1)'
-      }}>
-        <img src='/assets/Carrito/cerrar.svg' alt='close' className='absolute top-4 right-4 m-[20px] cursor-pointer' onClick={close}/>
+        className='bg-white w-full h-full lg:rounded-[54px] flex flex-col justify-center relative px-8'
+        style={{
+          boxShadow: '0 4px 6px rgba(0, 0, 0, 0.1)'
+        }}>
+        <img src='/assets/Carrito/cerrar.svg' alt='close' className='absolute top-4 right-4 m-[20px] cursor-pointer' onClick={close} />
         <span className='text-[24px] font-inter font-bold text-[#E39C9D]'>Filtrar</span>
-       {/*  <span className='text-[16px] font-inter font-semibold mt-[26px]'>Flores</span>
-      <div className="w-full  mt-[12px] ">
-        {floresOpen && (
-          <div className=" w-full flex flex-col gap-[15px]">
-            {flores.map((flower,index) => (
-              <Flor
-                key={index}
-                flower={flower}
-                checklist={checklist}
-                setChecklist={setChecklist}
-              />
-            ))}
-          </div>
-        )}
-      </div> */}
-
-      <div className='h-[1px] w-full bg-[#E39C9D] my-[26px]'/>
-
-     
-      <span className='text-[16px] font-inter font-semibold'>Tipo de arreglo</span>
-        
-      <div className="w-full  mt-[12px] ">
-        {arreglosOpen && (
-          <div className=" w-full flex flex-col gap-[15px]">
-            {arreglos.map((arreglo, index) => (
-              <Arreglo
-              key={index}
-                arreglo={arreglo}
-                checklistArreglos={checklistArreglos}
-                setChecklistArreglos={setChecklistArreglos}
-              />
-            ))}
-          </div>
-        )}
-      </div>
-
-      <div className='h-[1px] w-full bg-[#E39C9D] my-[26px]'/>
-
-     {/* 
-      <span className='text-[16px] font-inter font-semibold'>Precio</span>
-       
-      {precioOpen && (
-        <div className="mt-[29px]">
-      <RangeSlider id="range-slider" minValue={0} maxValue={100}  onChange={value => setValue(value)} />
-            <div className="flex justify-between">
-
-           <span>${value[0]}</span>
-           <span>${value[1]}</span>
+        <div className='h-[1px] w-full bg-[#E39C9D] my-[26px]' />
+        <span className='text-[16px] font-inter font-semibold'>Tipo de arreglo</span>
+        <div className="w-full mt-[12px]">
+          {arreglosOpen && (
+            <div className="w-full flex flex-col gap-[15px]">
+              {arreglos.map((arreglo, index) => (
+                <Arreglo
+                  key={index}
+                  arreglo={arreglo}
+                  checklistArreglos={tempChecklistArreglos}
+                  setChecklistArreglos={setTempChecklistArreglos}
+                />
+              ))}
+            </div>
+          )}
         </div>
+        <div className='h-[1px] w-full bg-[#E39C9D] my-[26px]' />
+        <div className='w-full flex justify-center'>
+          <button
+            className="bg-[#E39C9D] w-[188px] h-[60px] rounded-[6px] text-[24px] font-bold font-inter mt-[43px]"
+            onClick={() => {
+              setChecklist(tempChecklist);
+              setChecklistArreglos(tempChecklistArreglos);
+              close();
+            }}
+          >
+            Filtrar
+          </button>
         </div>
-      )}
-      */}
-      <div className='w-full flex justify-center'>
-        <button className="bg-[#E39C9D] w-[188px] h-[60px] rounded-[6px] text-[24px] font-bold font-inter mt-[43px] "
-        onClick={()=>{
-          close()
-        }
-          }>
-          Filtrar
-        </button>
-      </div> 
-
       </div>
     </div>
   );
 }
 
-export default FiltrosModal
+export default FiltrosModal;
